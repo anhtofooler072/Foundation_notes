a99d9166bb0ee4f61eb35b3a4af3356749a4e118
QuangAnhTo <quangato072@gmail.com>
2025-02-27 21:38:32 +0700

    feat: enhance C_PhiCalculator with optional plotting and messaging feat: calculate all the files in directory automatically in notebook

--- GEO_EDAs/utils.py
+++ GEO_EDAs/utils.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 
-def C_PhiCalulator(csv):
+def C_PhiCalulator(csv, plot=False, message=False):
     # Read the CSV file
     data = pd.read_csv(csv)
 
     # Display the first few rows of the dataframe to ensure it's loaded correctly
-    print(data.head())
+    # print(data.head())
 
     # Flatten the data
     normal_stress = np.repeat(data.columns.astype(float).values, data.shape[0]).reshape(-1, 1)
@@ -22,16 +22,18 @@ def C_PhiCalulator(csv):
     tan_phi = reg.coef_[0]  # Slope, tan(φ)
     phi = np.arctan(tan_phi)  # Friction angle
 
-    print(f"Cohesion (c): {np.round(c,3)}")
-    print(f"Friction angle (φ): {np.round(np.degrees(phi),3)}")
+    if message:
+        print(f"Cohesion (c): {np.round(c,3)}")
+        print(f"Friction angle (φ): {np.round(np.degrees(phi),3)} \n")
 
     #Plot the data and the regression line
-    plt.scatter(normal_stress, shear_resistance, color='blue')
-    plt.plot(normal_stress, reg.predict(normal_stress), color='red')
-    plt.xlabel('Normal Stress')
-    plt.ylabel('Shear Stress')
-    plt.title('Shear Stress vs. Normal Stress')
-    plt.show()
+    if plot:
+        plt.scatter(normal_stress, shear_resistance, color='blue')
+        plt.plot(normal_stress, reg.predict(normal_stress), color='red')
+        plt.xlabel('Normal Stress')
+        plt.ylabel('Shear Stress')
+        plt.title('Shear Stress vs. Normal Stress')
+        plt.show()
 
     return c, phi
 
